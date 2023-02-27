933696cb6faca0e103f0014e802cdb94e95cc40c
AkakiGaboshvili <125743940+AkakiGaboshvili@users.noreply.github.com>
2023-02-27 23:21:33 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,114 @@
+// const user = {
+//     name: "ako",
+//     surName: "gaboshvili",
+//     userName: "007",
+
+// }
+
+// const newUser = user
+// newUser.userName = "Agent 007"
+
+// console.log(user.userName);
+
+// let age = 30;
+
+
+
+// function generateRandomNumber(){
+//     return parseInt(Math.random() * 10) + 1
+// }
+
+// let randomNumber = generateRandomNumber()
+// while (randomNumber !== 7) {
+//     randomNumber= generateRandomNumber();
+//     console.log(randomNmber);
+// }
+
+// let randomNmber = generateRandomNumber()
+// console.log(randomNumber);
+// while(randomNmber !== 7)
+
+// const numbers = [1, 2, 5, 4, 3]
+// numbers.forEach(function(item, index) {
+// console.log (`$(index}) - $(item})`);
+// })
+
+
+// numbers.map(function(myNumber, index){
+//     return`$(index} => ${numbers}`
+// })
+
+// numbers.filter(function())
+
+
+// const person0 = { name: "Temo", age: 25 };
+// const person1 = { name: "Lasha", age: 21 };
+// const person2 = { name: "Ana", age: 28 };
+// console.log(Object.keys(person0), Object.keys(person1), Object.keys(person2));
+// console.log(person0.age < person1.age < person2.age);
+// function checkAge() {
+
+//     person0.age === person1.age === person2.age
+//     return age > 20
+
+// }
+// //
+// let user1 = { age: 25, name: "ako" }
+
+// function changeName(user1) {
+
+//     user1.name = newName
+//     newName = "lasha"
+
+
+// }
+// console.log()
+// //
+
+// const user = {
+//     name: "ako",
+//     surName: "gaboshvili",
+//     userName: "007",
+
+// }
+
+// const newUser = user
+// newUser.userName = "Agent 007"
+
+// console.log(user.userName);
+// //
+
+
+// function rollDice() {
+//     let roll = Math.floor(Math.random() * 6) + 1;
+//     for(let i = 0; i < 5; i++)
+//     if (rollDice = 3){
+//         console.log("winner");
+//     }
+//     console.log(`Rolled: ${roll}`);
+    
+// }
+
+// function throwDice() {
+    
+//     rollDice();
+//     rollDice();
+//     rollDice();
+//     rollDice();
+//     rollDice();
+//     rollDice();
+// }
+
+// console.log(throwDice());
+
+let diceRoller = function (num) {
+    let x = Math.floor(1 + (num * Math.random()));
+}
+
+let simulator = function (num){
+    for (let i = 0; i < num; i++){
+
+    }
+}
+simulator(10)
+
